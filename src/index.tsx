4c642e4488411fa5c7ab71d3edf47c2048b9083d
Sander Mack-Crane <smackcrane@gmail.com>
2025-01-05 09:55:27 -0700

    navigation context

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,55 @@
+import { createContext, useContext, useState } from "react"
 import ReactDOM from "react-dom/client"
 
+export { NavigationContext as PageContext }
+
+renderWebsite(<Website />)
+
 function renderWebsite(website: React.ReactNode) {
+    console.log("Rendering website")
     ReactDOM.createRoot(document.getElementById("root")!).render(website)
 }
 
-function App() {
+type NavigationProps = {
+    page: string
+    setPage: (page: string) => void
+}
+
+const NavigationContext = createContext<NavigationProps>({
+    page: "foo",
+    setPage: () => {},
+})
+
+function Website() {
+    const [page, setPage] = useState("home")
     return (
         <>
-            <h1>Hello, world!</h1>
-            {
-                "A website written in Typescript and React, and bundled with Vite."
-            }
+            <NavigationContext.Provider
+                value={{
+                    page: page,
+                    setPage: (page) => {
+                        console.log(page)
+                        setPage(page)
+                    },
+                }}
+            >
+                <DynamicPage />
+            </NavigationContext.Provider>
         </>
     )
 }
 
-renderWebsite(<App />)
+function DynamicPage() {
+    const nav = useContext(NavigationContext)
+    return (
+        <>
+            <h1>Hello, {nav.page}!</h1>
+            <p>
+                A website written in Typescript and React, and bundled with
+                Vite.
+            </p>
+            <button onClick={() => nav.setPage("home")}>Home</button>
+            <button onClick={() => nav.setPage("world")}>World</button>
+        </>
+    )
+}
