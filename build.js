3894d276ede222a23988fe00a8911aaa92e1ddef
Sander Mack-Crane <smackcrane@gmail.com>
2025-08-18 06:18:47 -0600

    reorg & build.js

---
+++ build.js
@@ -0,0 +1,55 @@
+import esbuild from "esbuild";
+import mdx from "@mdx-js/esbuild";
+import fs from "fs";
+import path from "path";
+import { renderToString } from "react-dom/server";
+
+// prior to build, clean out dist and out
+fs.rmSync("out", { recursive: true, force: true });
+fs.mkdirSync("out");
+fs.rmSync("dist", { recursive: true, force: true });
+fs.mkdirSync("dist");
+
+// bundle mdx files
+await esbuild.build({
+  entryPoints: ["pages/*.mdx"],
+  outdir: "out",
+  bundle: true,
+  format: "esm",
+  platform: "node",
+  //   plugins: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] })],
+  plugins: [mdx()],
+  //   loader: { ".js": ".jsx" },
+});
+
+// we want to produce a page for each file in pages/ dir
+const pages = fs.readdirSync("pages");
+fs.mkdirSync("dist/pages");
+for (const page of pages) {
+  const extension = path.extname(page);
+  const slug = path.basename(page, extension);
+
+  let html = "";
+  switch (extension) {
+    case ".html":
+      // in case of .html, just copy it
+      html = fs.readFileSync("pages/" + page);
+      break;
+    case ".mdx":
+      // in case of .mdx, import the bundled version from out/ and convert to html
+      const { default: Content } = await import(`./out/${slug}.js`);
+      html = renderToString(Content({ components: {} }));
+      break;
+    default:
+      continue; // ignore files that aren't .html or .mdx
+  }
+  // write to dist
+  fs.writeFileSync(`dist/pages/${slug}.html`, html);
+}
+
+// copy media/ and static/
+fs.cpSync("media", "dist/media", { recursive: true });
+fs.cpSync("static", "dist/static", { recursive: true });
+
+// special treatment for index.html
+fs.cpSync("index.html", "dist/index.html");
